8c5cf9770af68d5eb145cce4e9839b2f2089ee95
Syed Hussnain Shah <itshussnain787@gmail.com>
2022-07-19 11:45:55 +0500

    update

---
+++ 05-07-22/login.php
@@ -0,0 +1,42 @@
+<?php
+session_start();
+include_once 'header.php';
+?>
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-6 offset-3">
+            <center>
+                <h3>Login Form</h3>
+                <div class="alert alert-danger">
+                    <?php
+                    if (empty($_SESSION['message'])) {
+                        echo "No Message";
+                    } else {
+
+                        echo $_SESSION['message'];
+                        unset($_SESSION['message']);
+                    }
+                    ?>
+                </div>
+            </center>
+            <form action="Function.php" method="POST">
+                <label for="" class="w-100">
+                    Enter Your Email
+                    <input type="email" class="form-control" name="useremail" placeholder="Enter Your Name">
+                </label>
+                <label for="" class="w-100">
+                    Enter Your password
+                    <input type="password" class="form-control" name="userpassword" placeholder="Enter Your Name">
+                </label>
+                <input type="submit" name="loginform" class="btn btn-primary mt-2">
+            </form>
+        </div>
+    </div>
+
+</div>
+
+
+
+
+<?php include_once 'footer.php'; ?>
---
+++ 18-07-22/Function.php
@@ -0,0 +1,53 @@
+<?php
+session_start();
+include_once 'connection.php';
+if (isset($_POST['formsubmit'])) {
+    $name =  $_POST['username'];
+    $email =   $_POST['useremail'];
+    $password =  $_POST['userpassword'];
+    $pass = password_hash($password, PASSWORD_DEFAULT);
+    $SelectST = "SELECT * FROM signup WHERE user_email ='$email'";
+    $QueryST = mysqli_query($conn, $SelectST);
+    if (mysqli_num_rows($QueryST) == 1) {
+        echo "DATA Already";
+    } else {
+        $INSERT = "INSERT INTO signup (user_name,user_email,user_pass) VALUES ('$name','$email','$pass')";
+        $query = mysqli_query($conn, $INSERT);
+        if ($query) {
+            header('location:login.php');
+        } else {
+            echo mysqli_error($conn);
+        }
+    }
+}
+if (isset($_REQUEST['formupdate'])) {
+    $name =  $_POST['username'];
+    $email =   $_POST['useremail'];
+    $user_id = $_REQUEST['user_id'];
+    $update = "UPDATE `signup` SET `user_name`='$name',`user_email`='$email' WHERE id= '$user_id'";
+    $query = mysqli_query($conn, $update);
+    if ($query) {
+        header('location: index.php');
+    } else {
+        echo "Not Update";
+    }
+}
+if (isset($_REQUEST['loginform'])) {
+    $email = $_REQUEST['useremail'];
+    $pass = $_REQUEST['userpassword'];
+    $select = "SELECT * FROM signup WHERE user_email = '$email' AND user_pass = '$pass'";
+    $query = mysqli_query($conn, $select);
+    $row = mysqli_fetch_array($query);
+    if (mysqli_num_rows($query) > 0) {
+        if ($row['status'] == 'Admin') {
+            $_SESSION['user_id'] = $row['id'];
+            header('location:Dasboard/index.php');
+        } else {
+            $_SESSION['user_id'] = $row['id'];
+            header('location:index.php');
+        }
+    } else {
+        $_SESSION['message'] = "Email And Password Are Not Match<a href='index.php'>Please Reg Your Account</a>";
+        header('location: login.php');
+    }
+}
